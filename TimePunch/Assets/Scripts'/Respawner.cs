2c79c253f8d2708e9f814722ff9ceb5695213efd
spalunki <axisac72@gmail.com>
2018-01-27 18:59:26 -0500

    respawner

---
+++ TimePunch/Assets/Scripts'/Respawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawner : MonoBehaviour {
+    private Vector3 respawnPoint;
+    private GameObject[] platforms;
+
+	// Use this for initialization
+	void Start () {
+        respawnPoint = GameObject.FindGameObjectWithTag("Respawn").transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        platforms = GameObject.FindGameObjectsWithTag("Platform");
+
+        foreach (GameObject p in platforms)
+            if (this.transform.position.y < p.transform.position.y - 40f)
+                this.transform.position = respawnPoint;
+	}
+}
