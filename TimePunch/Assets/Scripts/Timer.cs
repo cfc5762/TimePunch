738500ee4ca12fafc438b732cd6f3e8c70867ffa
spalunki <axisac72@gmail.com>
2018-02-18 15:03:29 -0500

    Started Timer

---
+++ TimePunch/Assets/Scripts/Timer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+    float timeSoFar;
+    public Font f;
+	// Use this for initialization
+	void Start () {
+        timeSoFar = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeSoFar += Time.deltaTime;
+
+	}
+
+    void OnGUI()
+    {
+        GUI.skin.font = f;
+        
+        float minutes = Mathf.Floor(timeSoFar / 60);
+        float seconds = timeSoFar % 60;
+        
+        if (Mathf.RoundToInt(seconds)< 10)
+            GUI.Label(new Rect(Screen.width * 0.45f, 30, 250, 100), minutes + ":0" + Mathf.RoundToInt(seconds));
+        else
+            GUI.Label(new Rect(Screen.width*0.45f, 30, 250, 100), minutes + ":" + Mathf.RoundToInt(seconds));
+
+    }
+}
