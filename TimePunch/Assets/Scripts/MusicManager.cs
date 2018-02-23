66c7bab9e027ad7976428f7d3ffb03c3f53ca2cb
James Licata <jimmays.jl@gmail.com>
2018-02-23 17:57:58 -0500

    Added music and fixed textures

---
+++ TimePunch/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DigitalRuby.SoundManagerNamespace;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioSource[] MusicAudioSources;
+    public Toggle PersistToggle;
+
+
+    private void PlayMusic(int index)
+    {
+        MusicAudioSources[index].PlayLoopingMusicManaged(1.0f, 1.0f, PersistToggle.isOn);
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
