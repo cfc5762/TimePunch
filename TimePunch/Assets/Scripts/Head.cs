acc4b13b4f8071da12fd157f3c2754a60fa3caac
Chris Commisso <qwertykeebored@live.com>
2018-03-25 18:05:48 -0400

    player changes
    woo

---
+++ TimePunch/Assets/Scripts/Head.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Head : MonoBehaviour {
+    public static Vector3 Headpos;
+    public static Vector3 lookDir;
+    public static Vector3 rightDir;
+    Vector3 temp;
+	// Use this for initialization
+	void Start () {
+        rightDir = Vector3.zero;
+        lookDir = Vector3.zero;
+        temp = Vector3.zero;
+        Headpos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Headpos = transform.position;
+        temp = transform.forward;
+        temp.y = 0;
+        lookDir = temp.normalized;
+        temp = transform.right;
+        temp.y = 0;
+        rightDir = temp.normalized;
+	}
+}
